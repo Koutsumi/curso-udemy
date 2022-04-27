ceac61a4e7f4c37e9ab543f008a333ed19a38dfa
Fernanda <febaccarini@gmail.com>
2022-04-27 16:48:59 -0300

    46 teste dinamico n finalizado

---
+++ cypress/integration/dinamic.spec.js
@@ -0,0 +1,34 @@
+import { it } from "mocha";
+import PaginaPage from "../../pages/PaginaPage"
+
+describe('Comands', function(){
+    before(function(){
+        PaginaPage.go();
+    })
+
+    beforeEach(function(){
+        cy.reload();
+    })
+
+    const food = ['Carne', 'Frango', 'Pizza', 'Vegetariano']
+
+    it('cadastro com comida variada', function(){
+
+            cy.wrap(food).each(function($el){
+                it(`variação ${food}`, function(){
+                    cy.get('#formNome').type(this.usuario.nome)
+                    cy.get('#formSobrenome').type(this.usuario.sobrenome)
+                    cy.get(`[name="formSexo"][value=${this.usuario.sexo}]`).click()
+                    cy.xpath(`//label[contains(.,${$el})]/preceding-sibling::input`).click()
+                    cy.get('#formEscolaridade').select(this.usuario.escolaridade)
+                    cy.get('#formEsportes').select(this.usuario.esportes)
+                    cy.get('#formCadastrar').click()
+
+                })
+                
+            })
+            
+        
+        
+    })
+})
